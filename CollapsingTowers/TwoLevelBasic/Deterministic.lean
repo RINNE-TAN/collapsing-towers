fee0a543e727382aa58d9727f356373ddeab959a
kamome <2038975825@qq.com>
2025-07-23 16:17:18 +0800

    feat: decompose_deterministic

--- CollapsingTowers/TwoLevelBasic/Deterministic.lean
+++ CollapsingTowers/TwoLevelBasic/Deterministic.lean
@@ -1,10 +1,49 @@
 
 import CollapsingTowers.TwoLevelBasic.SmallStep
+theorem decompose𝔹_deterministic :
+  ∀ e₀ e₁ B₀ B₁,
+    ctx𝔹 B₀ →
+    ctx𝔹 B₁ →
+    B₀⟦e₀⟧ = B₁⟦e₁⟧ →
+    ¬value e₀ →
+    ¬value e₁ →
+    e₀ = e₁ ∧ B₀ = B₁ :=
+  by
+  intros e₀ e₁ B₀ B₁ HB₀ HB₁ HEq HNv₀ HNv₁
+  cases HB₀ <;>
+  cases HB₁ <;>
+  (simp at HEq; try simp [HEq]) <;>
+  exfalso <;>
+  (try apply HNv₀; simp [HEq]; assumption) <;>
+  (try apply HNv₁; simp [← HEq]; assumption)
+
+theorem decomposeℝ_deterministic :
+  ∀ e₀ e₁ lvl intro₀ intro₁ R₀ R₁,
+    ctxℝ intro₀ lvl R₀ →
+    ctxℝ intro₁ lvl R₁ →
+    R₀⟦e₀⟧ = R₁⟦e₁⟧ →
+    lc e₀ →
+    lc e₁ →
+    ¬value e₀ →
+    ¬value e₁ →
+    e₀ = e₁ ∧ intro₀ = intro₁ ∧ R₀ = R₁ :=
+  by
+  intros e₀ e₁ lvl intro₀ intro₁ R₀ R₁ HR₀ HR₁ HEq Hlc₀ Hlc₁ HNv₀ HNv₁
+  cases HR₀ <;>
+  cases HR₁ <;>
+  (simp at HEq; try simp [HEq])
+  case lam𝕔.lam𝕔 =>
+    rw [← open_close_id _ _ _ Hlc₀, ← open_close_id _ _ _ Hlc₁]
+    rw [HEq]
+  case let𝕔.let𝕔 =>
+    rw [← open_close_id _ _ _ Hlc₀, ← open_close_id _ _ _ Hlc₁]
+    rw [← HEq.right]
+
 theorem deterministic :
-  ∀ e₀ e₁ e₂,
-    step e₀ e₁ →
-    step e₀ e₂ →
-    e₁ = e₂ :=
+  ∀ e l r,
+    step e l →
+    step e r →
+    l = r :=
   by admit
 
 theorem church_rosser_strengthened :
