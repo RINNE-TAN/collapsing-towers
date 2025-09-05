3d18e5b3410111de7782681348ce49b98a71364a
kamome <2038975825@qq.com>
2025-09-05 14:48:48 +0800

    feat: mut ctx equiv trans

--- CollapsingTowers/TwoLevelMut/CtxEquiv/Defs.lean
+++ CollapsingTowers/TwoLevelMut/CtxEquiv/Defs.lean
@@ -1,1 +1,2 @@
 import CollapsingTowers.TwoLevelMut.CtxEquiv.ObsCtx
+import CollapsingTowers.TwoLevelMut.CtxEquiv.Transitivity
--- CollapsingTowers/TwoLevelMut/OperationalSemantics/Defs.lean
+++ CollapsingTowers/TwoLevelMut/OperationalSemantics/Defs.lean
@@ -4,3 +4,4 @@ import CollapsingTowers.TwoLevelMut.OperationalSemantics.EvalCtx
 import CollapsingTowers.TwoLevelMut.OperationalSemantics.SmallStep
 import CollapsingTowers.TwoLevelMut.OperationalSemantics.Congruence
 import CollapsingTowers.TwoLevelMut.OperationalSemantics.Deterministic
+import CollapsingTowers.TwoLevelMut.OperationalSemantics.Confluence
