b32ea7a97e27428cc6c8c05bea0830e34784f203
kamome <2038975825@qq.com>
2025-10-13 21:15:27 +0800

    chore: clean up code

--- CollapsingTowers/TwoLevelMut/Defs.lean
+++ CollapsingTowers/TwoLevelMut/Defs.lean
@@ -2,6 +2,3 @@ import CollapsingTowers.TwoLevelMut.Utils.Defs
 import CollapsingTowers.TwoLevelMut.Syntax.Defs
 import CollapsingTowers.TwoLevelMut.OperationalSemantics.Defs
 import CollapsingTowers.TwoLevelMut.SyntacticTyping.Defs
-import CollapsingTowers.TwoLevelMut.SyntacticSoundness.Defs
-import CollapsingTowers.TwoLevelMut.CtxEquiv.Defs
-import CollapsingTowers.TwoLevelMut.LogicalEquiv.Defs
