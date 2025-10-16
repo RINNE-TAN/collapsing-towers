3e2a849d93a342039a06c23ac2f423c74c2752ee
kamome <2038975825@qq.com>
2025-10-16 14:45:44 +0800

    feat: effect union lemma

--- CollapsingTowers/TwoLevelMut/SyntacticTyping/Effect.lean
+++ CollapsingTowers/TwoLevelMut/SyntacticTyping/Effect.lean
@@ -1,4 +1,5 @@
 import Mathlib.Order.Basic
+import Mathlib.Data.Set.Image
 
 inductive Stage : Type where
   | static
@@ -36,7 +37,7 @@ abbrev Effects :=
 
 @[simp]
 def erase_effects (φ : Effects) : Effects :=
-  { erase_effect β | β ∈ φ } \ { .reify }
+  { erase_effect β | β ∈ φ \ { .reify } }
 
 @[simp]
 def escape_effects (φ : Effects) : Effects :=
@@ -49,8 +50,45 @@ def wbt_effects (𝕊 : Stage) (φ : Effects) : Prop :=
 lemma grounded_effects.under_erase : ∀ φ, wbt_effects 𝟚 (erase_effects φ) :=
   by
   intros φ β₀ Hβ₀
-  have ⟨Hmutate, Hreify⟩ := Hβ₀
-  have ⟨β₁, _, HEqβ⟩ := Hmutate
+  have ⟨β₁, Hβ₁, HEqβ⟩ := Hβ₀
   rw [← HEqβ]; apply grounded_effect.under_erase
-  intros Hβ₁; apply Hreify
-  simp [Hβ₁] at HEqβ; simp [HEqβ]; rfl
+  apply Hβ₁.right
+
+@[simp]
+def erase_effects.union : ∀ φ₀ φ₁, erase_effects (φ₀ ∪ φ₁) = erase_effects φ₀ ∪ erase_effects φ₁ :=
+  by
+  intros φ₀ φ₁
+  simp only [erase_effects]
+  repeat rw [← Set.image]
+  rw [← Set.image_union, Set.union_diff_distrib]
+
+@[simp]
+def erase_effects.reify : erase_effects { .reify } = ∅ :=
+  by simp
+
+@[simp]
+lemma erase_effects.mutate : ∀ 𝕊, erase_effects { .mutate 𝕊 } = { .mutate 𝟚 } :=
+  by simp
+
+@[simp]
+def erase_effects.diff_reify : ∀ φ, erase_effects (φ \ { .reify }) = erase_effects φ :=
+  by simp
+
+@[simp]
+def wbt_effects.union : ∀ 𝕊 φ₀ φ₁, wbt_effects 𝕊 (φ₀ ∪ φ₁) ↔ (wbt_effects 𝕊 φ₀ ∧ wbt_effects 𝕊 φ₁) :=
+  by
+  intros 𝕊 φ₀ φ₁
+  constructor
+  . intros Hφ
+    constructor
+    . intros β Hβ
+      apply Hφ
+      apply Set.mem_union_left _ Hβ
+    . intros β Hβ
+      apply Hφ
+      apply Set.mem_union_right _ Hβ
+  . intros Hφ
+    intros β Hβ
+    cases Hβ
+    case inl Hβ => apply Hφ.left _ Hβ
+    case inr Hβ => apply Hφ.right _ Hβ
